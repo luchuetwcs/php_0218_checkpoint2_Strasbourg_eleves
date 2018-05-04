6f109d334729ba86580bdd96fc7478e7a1675118
Tiffanym <tiffany.maurath@orange.fr>
2018-05-04 14:08:50 +0200

    Rien de neuf

--- src/Service/StringTools.php
+++ src/Service/StringTools.php
@@ -10,7 +10,13 @@ class StringTools
 
     public static function trimWhiteSpaces(string $str):string
     {
-        //TODO Write your code here,
-        //TODO And return something at the end...
-    }
+        $str_arr = explode(" ", $str);
+
+        $clear = implode("", $str_arr);
+
+        $hello= substr($clear, 0, -6);
+        $wilder= substr($clear, 5);
+
+        return $hello . " " . $wilder;
+
 }
