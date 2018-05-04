f17919238b4372e23fc66abeda14f607e0e043e2
Jennifer Lieby <ze_nifer@hotmail.com>
2018-05-04 09:19:05 +0200

    first commit

---
+++ src/Service/StringTools.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Service;
+
+/**
+ *  class StringTools
+ */
+class StringTools
+{
+
+    public static function trimWhiteSpaces(string $str):string
+    {
+        //TODO Write your code here,
+        //TODO And return something at the end...
+    }
+}
