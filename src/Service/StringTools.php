7e47b99e602b7e0ac2692c687bea1db5566fc1ea
cpailler <aprel.illectemn@gmail.com>
2018-05-04 10:18:56 +0200

    alogorithme exercice

--- src/Service/StringTools.php
+++ src/Service/StringTools.php
@@ -10,7 +10,9 @@ class StringTools
 
     public static function trimWhiteSpaces(string $str):string
     {
-        //TODO Write your code here,
-        //TODO And return something at the end...
+        while ($str[0] == " ") {
+            $str = substr($str, 1, strlen($str) - 1);
+        }
+        return $str;
     }
 }
